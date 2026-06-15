f7684700120a820bf67dabc327ef13d54e5f06a7
unknown <yannick@eisenblaetter-net.de>
2026-06-15 16:40:35 +0200

    Aufgabe 5.3

---
+++ TestNulldurchgang/TestNulldurchgang.ino
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+
+
+// Pins definieren
+const int PIN_TASTER1     = 0;
+const int PIN_TASTER2     = 1;
+const int PIN_TRIAC       = 3;  
+const int PIN_ZC          = 6;  
+const int PIN_ZC_INT      = 9;  
+const int PIN_LED1        = 14;
+const int PIN_LED2        = 15;
+
+
+void setup(){
+  // Pins konfigurieren
+  pinMode(PIN_TASTER1, INPUT);
+  pinMode(PIN_TASTER2, INPUT);
+  pinMode(PIN_TRIAC, OUTPUT);
+  pinMode(PIN_ZC_INT, INPUT);
+  pinMode(PIN_LED1, OUTPUT);
+  pinMode(PIN_LED2, OUTPUT);
+  digitalWrite(PIN_TRIAC, LOW);
+  digitalWrite(PIN_LED1, LOW);
+  digitalWrite(PIN_LED2, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(PIN_ZC_INT), RisingEdgeDetected, RISING)
+
+}
+
+void RisingEdgeDetected(){
+  digitalWrite(PIN_TRIAC, HIGH);
+  // zwei Millisekunden aber in DelayMicroseconds, da das keinen Counter braucht --> Fehler von bis zu 4% möglich, hier aber nicht relevant
+  delayMicroseconds(2000)
+  digitalWrite(PIN_TRIAC, LOW);
+}
+
+
+void loop(){
+}
